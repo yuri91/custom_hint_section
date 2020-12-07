288ed91e821583b68baae3a97fedda9492bc358c
Yuri Iozzelli <y.iozzelli@gmail.com>
2020-12-07 14:52:24 +0100

    Hint false, not true

--- main.js
+++ main.js
@@ -62,7 +62,7 @@ for (let [fidx, fentry] of info.entries()) {
 	append(data, u32toULEB(fidx));
 	append(data, u32toULEB(fentry.length));
 	for (let [idx, entry] of fentry.entries()) {
-		append(data, u32toULEB(2));
+		append(data, u32toULEB(1));
 	}
 }
 
