a2a9cb0f8272997196a6b9633efc3bc0858cb221
Yuri Iozzelli <y.iozzelli@gmail.com>
2021-02-23 16:56:53 +0100

    Assign hints based on a text file

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use anyhow::Result;
 use std::env;
 use std::io::Write;
+use std::collections::HashMap;
 use wasmparser::{Parser, Payload, Operator};
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
 enum HintDirection {
     True,
     False,
@@ -53,14 +54,36 @@ impl BranchHintsSection {
     }
 }
 
+fn parse_file(path: &str) -> Result<HashMap<u32, HashMap<u32, HintDirection>>> {
+    let content = std::fs::read_to_string(path)?;
+    let mut ret: HashMap<u32, HashMap<_,_>> = HashMap::new();
+    let mut cur = 0u32;
+    for l in content.lines() {
+        if l.starts_with('\t') {
+            let space = l.find(' ').unwrap()-1;
+            let (branch_n, branch_v) = l.trim_start_matches('\t').split_at(space);
+            let hint = match branch_v.trim().parse::<u32>()? {
+                0 => HintDirection::False,
+                1 => HintDirection::True,
+                _ => unreachable!()
+            };
+            ret.get_mut(&cur).unwrap().insert(branch_n.parse()?, hint);
+        } else {
+            cur = l.parse()?;
+            ret.insert(cur, HashMap::new());
+        }
+    }
+    Ok(ret)
+}
 fn main() -> Result<()> {
     let args = env::args().collect::<Vec<_>>();
-    if args.len() != 3 {
-        println!("Usage: {} in.wasm out.wasm", args[0]);
+    if args.len() != 4 {
+        println!("Usage: {} in.wasm out.wasm hints.txt", args[0]);
         return Ok(());
     }
 
     let buf: Vec<u8> = std::fs::read(&args[1])?;
+    let hints = parse_file(&args[3])?;
     let mut cur_func_idx = 0;
     let mut section = BranchHintsSection{funcs: Vec::new()};
     let mut code_start = 0;
@@ -73,33 +96,33 @@ fn main() -> Result<()> {
                 let mut size_buf = Vec::new();
                 leb128::write::unsigned(&mut size_buf, size as u64)?;
                 code_start = range.start - 1 - size_buf.len();
-                println!("code start {:x} {:?}", code_start, &buf[code_start..range.end]);
             }
             Payload::CodeSectionEntry(body) => {
                 let op_reader = body.get_operators_reader()?;
                 let offset_start = op_reader.original_position() as u32;
-                println!("offset_start {:x}", offset_start);
                 let mut info = FuncBranchInfo{func: cur_func_idx, branches: Vec::new()};
                 for r in op_reader.into_iter_with_offsets() {
                     let (op, off) = r?;
-                    println!("{:?} {:x}", op, off);
-                    let off = off as u32;
+                    let offset = off as u32 - offset_start;
                     let dir = match op {
-                        Operator::BrIf{..} => {
-                            HintDirection::True
-                        }
-                        Operator::If{..} => {
-                            HintDirection::False
+                        Operator::BrIf{..}|Operator::If{..} => {
+                            println!("f {} br {}", cur_func_idx, offset);
+                            hints.get(&cur_func_idx).and_then(|m| m.get(&offset))
                         }
                         _ => {
                             continue;
                         }
                     };
-                    info.branches.push(BranchInfo{dir, offset:off-offset_start});
+                    let dir = match dir {
+                        Some(d) => d,
+                        None => continue
+                    };
+                    info.branches.push(BranchInfo{dir:*dir, offset});
                 }
                 if !info.branches.is_empty() {
                     section.funcs.push(info);
                 }
+                cur_func_idx+=1;
             }
             _ => {
             }
